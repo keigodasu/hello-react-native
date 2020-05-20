07af7f291ca5225e7242bb4ca14ab069cf826d01
kei <kei@keinoMacBook-puro.local>
2020-05-20 12:36:09 +0900

    complete chap7

---
+++ NewMyApp/src/components/pages/Loading/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import * as UiContext from '../../../contexts/ui';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+function ChangeStateButton(props: { state: UiContext.Status }) {
+  const { setApplicationState } = React.useContext(UiContext.Context);
+  const { state } = props;
+  return (
+    <TouchableOpacity onPress={() => setApplicationState(state)}>
+      <Text>Change state to {state}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.UN_AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.FIRST_OPEN} />
+    </View>
+  );
+}
